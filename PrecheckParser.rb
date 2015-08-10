8988a3a59a06f5beb69318388519c9811e0f304c
Will Thammavong <thammavo@eng.utah.edu>
2015-08-10 00:05:38 -0600

    nil check for first argument

--- PrecheckParser.rb
+++ PrecheckParser.rb
@@ -2,13 +2,26 @@
 
 # Author: William Thammavong
 # Date: 08/07/2015
-# Will be given input text file containing Pre check health check. That text file will then get parsed into a more friendly manner to input to notes.
+# Will be given input text file containing Pre check health check.
+# That text file will then get parsed into a more friendly manner to input to notes.
 
 
 textFileInput = ARGV[0]
 textFileOutput = ARGV[1]
 
-inputText = File.open(textFileInput)
+if (textFileInput != nil)
+  inputText = File.open(textFileInput)
+else
+  puts 'You have not specified a text file as the first argument.'
+end
+
+
+
+
+
+
+
+
 
 puts inputText.read()
 
