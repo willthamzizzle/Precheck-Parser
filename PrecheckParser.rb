02739394a2b24125c88ec85a9cc77d40b4478347
Will Thammavong <thammavo@eng.utah.edu>
2015-08-12 00:44:30 -0600

    parsing files

--- PrecheckParser.rb
+++ PrecheckParser.rb
@@ -6,24 +6,18 @@
 # That text file will then get parsed into a more friendly manner to input to notes.
 
 
-textFileInput = ARGV[0]
-textFileOutput = ARGV[1]
+$textFileInput = ARGV[0]
 
-if (textFileInput != nil)
-  inputText = File.open(textFileInput)
-else
-  puts 'You have not specified a text file as the first argument.'
+
+
+def obtainText()
+
+  inputText = nil
+  if(File.exist?($textFileInput))
+    inputText = File.open($textFileInput)
+  end
+
+  puts inputText.read()
 end
 
-
-
-
-
-
-
-
-
-puts inputText.read()
-
-puts textFileOutput
-puts textFileInput
+obtainText()
